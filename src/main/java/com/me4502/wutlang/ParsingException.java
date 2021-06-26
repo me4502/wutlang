5609f9149ed72c480485f5eba7b25cbd1cddacf7
Madeline Miller <mnmiller1@me.com>
2021-06-26 17:09:50 +1000

    It's $currentYear

--- src/main/java/com/me4502/wutlang/ParsingException.java
+++ src/main/java/com/me4502/wutlang/ParsingException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017 Me4502 (Madeline Miller)
+ * Copyright (c) 2016-2021 Me4502 (Maddy Miller)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
